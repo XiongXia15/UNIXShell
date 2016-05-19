268cfbcdc73d256a0ff8cbec4468dcd30de33a38
XiongXia15 <2272924180@qq.com>
2016-05-19 20:42:40 +0800

    order_name

--- shell.cpp
+++ shell.cpp
@@ -39,10 +39,71 @@ char *read_order(char *buffer)
 	return input;
 }
 
+int order_number(const char *input)
+{
+	int sum=0,i=0,len;
+	len=strlen(input);
+	while(i<len && (input[i] == ' ' || input[i] == '	'))
+		i++;
+	if(input[i] == ';')
+	{
+		fprintf(stderr,"Syntax Error. Unexpected token ';' \n");
+		return -1;
+	}
+	if(i == len)
+		return -1;
+	for(i=0;i<len;i++)
+		if(input[i] == ';')
+		{
+			while(i<strlen(input) && (input[i+1] == ' ' || input[i+1] == '	'))
+			i++;
+			if(input[i+1] == ';')
+			{
+				fprintf(stderr,"Syntax Error. Unexpected token ';;' \n")
+				return -1;
+			}
+			else
+				sum++;
+		}
+	sum=sum+1;
+	return sum;
+}
+
 char **order_name(const char *input)
 {
+//	int order_number(const char *input);
+	int i,j,k,max_len;
+	char **order;
 	
-
+	max_len=strlen(input);
+	k=order_number(input);
+	order=(char **)malloc(k*sizeof(char *));
+	for(i=0;i<k;i++)
+	{
+		order[i]=(char *)malloc((max_len+1)*sizeof(char));
+		order[i][0]='\0';
+	}
+	//分别取出被";"分割开的命令
+	k=0;
+	j=0;
+	for (i=0;i<=max_len;i++)
+	{
+		if (input[i]!=';')
+		{
+			order[k][j]=input[i];
+			j++;
+		}
+		else
+		{
+			order[k][j]='\0';
+			k++;
+			j=0;
+		}
+	}
+	//show the orders that are departed by ';'
+	/*for(i=0;i<k+1;i++)
+		printf("%s\n",order[i]);*/
+	return order;
 }
 
 int pipel(char *input)
